575e0b935203bb88c16bd14cdbee3885760e8fe2
javaheri2016 <Marker22%>
2020-12-06 21:48:20 +0100

    hackathon part 1

---
+++ README.md
@@ -0,0 +1,17 @@
+# Eyes Python Selenium Hackathon Applitools
+
+### Pre-requisites:
+
+1. Python 3 is installed on your machine.  [Install Python 3.](https://realpython.com/installing-python/) 
+2. Package manager pip is installed on your machine.  [Install pip](https://pip.pypa.io/en/stable/installing/)
+3. Chrome browser is installed on your machine. [Install Chrome browser](https://support.google.com/chrome/answer/95346?co=GENIE.Platform%3DDesktop&hl=en&oco=0)  
+4. Chrome Webdriver will be downloaded automatically via Webdriver Manager [Webdriver Manager](https://pypi.org/project/webdriver-manager/)
+5. Git is installed on your machine. [Install git](https://www.atlassian.com/git/tutorials/install-git)
+6. If you want to run example from IDE, install any IDE for Python (e.g. [PyCharm](https://www.jetbrains.com/pycharm/download/) )
+
+### Run the tests
+1. Open in any editor file `ultrafastgrid_demo.py ` and set your ApiKey in string `.set_api_key('...')`
+2. Install requirements `pip install -r requirements.txt`
+3. Run tests by calling `python ultrafastgrid_hackathon.py` 
+
+Read more here: https://www.applitools.com/tutorials/selenium-python.html
---
+++ ultrafastgrid_hackathon.py
@@ -0,0 +1,125 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import Chrome
+
+import os
+
+# key_applitools = os.environ.get('APPLITOOLS_API_KEY')
+
+from applitools.selenium import (
+    logger,
+    VisualGridRunner,
+    Eyes,
+    Target,
+    BatchInfo,
+    BrowserType,
+    DeviceName,
+)
+
+
+def set_up(eyes):
+
+    # You can get your api key from the Applitools dashboard
+    eyes.configure.set_api_key('APPLITOOLS_API_KEY')
+
+    # Create a new batch info instance and set it to the configuration
+    eyes.configure.set_batch(BatchInfo("Holiday Shopping"))
+
+    # Add browsers with different viewports
+    # Add mobile emulation devices in Portrait mode
+    (
+        eyes.configure.add_browser(1200, 800, BrowserType.CHROME)
+    )
+
+
+def main_page_test(web_driver, eyes):
+    try:
+        # Navigate to the url we want to test
+        web_driver.get("https://demo.applitools.com/tlcHackathonMasterV1.html")
+
+        # Call Open on eyes to initialize a test session
+        eyes.open(
+            web_driver, "AppliFashion", "Test 1"
+        )
+
+        # Check main page
+        eyes.check("", Target.window().fully().with_name("main page"))
+
+        # Call Close on eyes to let the server know it should display the results
+        eyes.close_async()
+    except Exception as e:
+        eyes.abort_async()
+        print(e)
+
+
+def filter_by_color_test(web_driver, eyes):
+    try:
+        # Navigate to the url we want to test
+        web_driver.get("https://demo.applitools.com/tlcHackathonMasterV1.html")
+
+        # Call Open on eyes to initialize a test session
+        eyes.open(
+            web_driver, "AppliFashion", "Test 2"
+        )
+
+        # Check filters
+        web_driver.find_element_by_id("LI____103").click()
+        web_driver.find_element_by_id("filterBtn").click()
+        eyes.check_region("#product_grid", "filter by color")
+
+        # Call Close on eyes to let the server know it should display the results
+        eyes.close_async()
+    except Exception as e:
+        eyes.abort_async()
+        print(e)
+
+
+def product_details_test(web_driver, eyes):
+    try:
+        # Navigate to the url we want to test
+        web_driver.get("https://demo.applitools.com/tlcHackathonMasterV1.html")
+
+        # Call Open on eyes to initialize a test session
+        eyes.open(
+            web_driver, "AppliFashion", "Test 3"
+        )
+
+        # Click on product
+        web_driver.find_element_by_id("H3____218").click()
+
+        # # Check the app page
+        eyes.check("", Target.window().fully().with_name("product details"))
+
+        # Call Close on eyes to let the server know it should display the results
+        eyes.close_async()
+    except Exception as e:
+        eyes.abort_async()
+        print(e)
+
+
+def tear_down(web_driver, runner):
+    # Close the browser
+    web_driver.quit()
+
+    # we pass false to this method to suppress the exception that is thrown if we
+    # find visual differences
+    all_test_results = runner.get_all_test_results()
+    print(all_test_results)
+
+
+# Create a new chrome web driver
+web_driver = Chrome(ChromeDriverManager().install())
+
+# Create a runner with concurrency of 1
+runner = VisualGridRunner(1)
+
+# Create Eyes object with the runner, meaning it'll be a Visual Grid eyes.
+eyes = Eyes(runner)
+
+set_up(eyes)
+
+try:
+    main_page_test(web_driver, eyes)
+    filter_by_color_test(web_driver, eyes)
+    product_details_test(web_driver, eyes)
+finally:
+    tear_down(web_driver, runner)
